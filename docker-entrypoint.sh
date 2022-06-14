661c0cd5eb02313cf53d03e76279992c1bb5b6f2
Evgeny Kolyakov <freaker2k7@users.noreply.github.com>
2022-06-14 16:44:36 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,7 +20,7 @@ strart_local_redis() {
       echo >&2 "installing redis-server.."
       apk add --no-cache -q redis
       echo >&2 "starting local redis-server.."
-      redis-server --daemonize yes
+      redis-server --daemonize yes --save "" --appendonly no --protected-mode no
     fi
   fi
 }
@@ -32,32 +32,20 @@ ACL_HTTP_BASIC_AUTH_ENABLED=${ACL_HTTP_BASIC_AUTH_ENABLED:-\"DEBUG\"}
 cat - <<EOF
 {
   "redis_host": "${REDIS_HOST:-redis}",
-
   "redis_port": ${REDIS_PORT:-6379},
   "redis_auth": ${REDIS_AUTH:-null},
-
   "http_host": "${HTTP_HOST:-0.0.0.0}",
   "http_port": ${HTTP_PORT:-7379},
-
   "threads": ${THREADS:-5},
   "pool_size": ${POOL_SIZE:-10},
-
   "daemonize": false,
   "websockets": ${WEBSOCKETS:-false},
-
   "database": ${DATABASE:-0},
-
   "acl": [
     {
-      "disabled": [${ACL_DISABLED}]
-    },
-
-    {
-      "http_basic_auth": "${ACL_HTTP_BASIC_AUTH:-user:password}",
-      "enabled":  [${ACL_HTTP_BASIC_AUTH_ENABLED}]
+      "enabled": ["FCALL"]
     }
   ],
-
   "verbosity": ${VERBOSITY:-99},
   "logfile": "${LOGFILE:-/dev/stdout}"
 }
